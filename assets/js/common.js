cdd9c5e240ce1a947c6d5188817348323ce4057c
bnf9910 <paik9910@gmail.com>
2024-04-22 14:28:31 +0900

    Modify language button, hide main page texts.

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,91 +1,87 @@
-window.onload = function() {
-    var currentPageUrl = window.location.href;
-    var fileName = currentPageUrl.substring(currentPageUrl.lastIndexOf('/') + 1);
-    
-    var secondLiA;
-    var intervalId = setInterval(function() {
-        if (fileName === "index.html") {
-            secondLiA = document.querySelector("header .container .gnb-menu ul li:nth-child(1) a");
-        } else if (fileName === "about.html") {
-            secondLiA = document.querySelector("header .container .gnb-menu ul li:nth-child(2) a");
-        } else if (fileName === "work.html") {
-            secondLiA = document.querySelector("header .container .gnb-menu ul li:nth-child(3) a");
+window.onload = function () {
+  var currentPageUrl = window.location.href;
+  var fileName = currentPageUrl.substring(currentPageUrl.lastIndexOf("/") + 1);
+
+  var secondLiA;
+  var intervalId = setInterval(function () {
+    if (fileName === "index.html") {
+      secondLiA = document.querySelector(
+        "header .container .gnb-menu ul li:nth-child(1) a"
+      );
+    } else if (fileName === "about.html") {
+      secondLiA = document.querySelector(
+        "header .container .gnb-menu ul li:nth-child(2) a"
+      );
+    } else if (fileName === "work.html") {
+      secondLiA = document.querySelector(
+        "header .container .gnb-menu ul li:nth-child(3) a"
+      );
+    }
+
+    if (secondLiA) {
+      secondLiA.classList.add("on");
+      clearInterval(intervalId);
+
+      var korean = document.getElementById("ko__btn"),
+        english = document.getElementById("en__btn"),
+        ko_txt = document.querySelectorAll("#ko"),
+        en_txt = document.querySelectorAll("#en"),
+        array_en = en_txt.length;
+      (array_ko = ko_txt.length),
+        korean.addEventListener(
+          "click",
+          function () {
+            langue(korean, english);
+          },
+          false
+        );
+      english.addEventListener(
+        "click",
+        function () {
+          langue(english, korean);
+        },
+        false
+      );
+      function langue(langueOn, langueOff) {
+        if (!langueOn.classList.contains("active_lang")) {
+          langueOn.classList.toggle("active_lang");
+          langueOff.classList.toggle("active_lang");
         }
-        
-        if (secondLiA) {
-            secondLiA.classList.add("on");
-            clearInterval(intervalId); 
 
-                        
-            var english = document.getElementById("en__btn"),
-            korean = document.getElementById("ko__btn"),
-            en_txt = document.querySelectorAll("#en"),
-            ko_txt = document.querySelectorAll("#ko"),
-            array_en = en_txt.length,
-            array_ko = ko_txt.length;
+        if (langueOn.innerHTML == "한국어") {
+          afficher(ko_txt, array_ko);
+          cacher(en_txt, array_en);
 
+          sessionStorage.setItem("selectedLanguage", "ko");
+        } else if (langueOn.innerHTML == "English") {
+          afficher(en_txt, array_en);
+          cacher(ko_txt, array_ko);
 
-            english.addEventListener(
-            "click",
-            function () {
-                langue(english, korean);
-            },
-            false
-            );
-            korean.addEventListener(
-            "click",
-            function () {
-                langue(korean, english);
-            },
-            false
-            );
-            function langue(langueOn, langueOff) {
-                if (!langueOn.classList.contains("active_lang")) {
-                    langueOn.classList.toggle("active_lang");
-                    langueOff.classList.toggle("active_lang");
-                }
-
-                if (langueOn.innerHTML == "English") {
-                    afficher(en_txt, array_en);
-                    cacher(ko_txt, array_ko);
-
-                    sessionStorage.setItem("selectedLanguage", "en");
-                } else if (langueOn.innerHTML == "한국어") {
-                    afficher(ko_txt, array_ko);
-                    cacher(en_txt, array_en);
-
-                    sessionStorage.setItem("selectedLanguage", "ko");
-                }
-            }
-            function afficher(txt, array) {
-                for (var i = 0; i < array; i++) {
-                    txt[i].style.display = "block";
-                }
-            }
-            function cacher(txt, array) {
-                for (var i = 0; i < array; i++) {
-                    txt[i].style.display = "none";
-                }
-            }
-            function init() {
-                langue(english, korean);
-            }
-
-            
-
-            var selectedLanguage = sessionStorage.getItem("selectedLanguage");
-            if (selectedLanguage === "en") {
-                langue(english, korean);
-            } else if (selectedLanguage === "ko") {
-                langue(korean, english);
-            } else {
-                init();
-            }
-
-
-            
-
-              
+          sessionStorage.setItem("selectedLanguage", "en");
         }
-    }, 100); // 100ms마다 함수 실행
+      }
+      function afficher(txt, array) {
+        for (var i = 0; i < array; i++) {
+          txt[i].style.display = "block";
+        }
+      }
+      function cacher(txt, array) {
+        for (var i = 0; i < array; i++) {
+          txt[i].style.display = "none";
+        }
+      }
+      function init() {
+        langue(korean, english);
+      }
+
+      var selectedLanguage = sessionStorage.getItem("selectedLanguage");
+      if (selectedLanguage === "ko") {
+        langue(korean, english);
+      } else if (selectedLanguage === "en") {
+        langue(english, korean);
+      } else {
+        init();
+      }
+    }
+  }, 100); // 100ms마다 함수 실행
 };
